f8e3080ce2ca5fe57ac664409239b69f25c9df56
Sam Vaughan <sam.vaughan@physics.ox.ac.uk>
2017-09-03 12:01:42 +0100

    Maybe now?

--- _posts/2017-09-03-D2_and_JavaScript.md
+++ _posts/2017-09-03-D2_and_JavaScript.md
@@ -6,10 +6,10 @@ categories: code
 ---
 
 Test
+
 <script src="http://d3js.org/d3.v4.min.js"></script>
 <script src="https://raw.githubusercontent.com/samvaughan/samvaughan.github.io/master/_posts/JS/drag_points.js"></script>
 
-</script>
 
 <div id="example"></div>
 
--- _posts/JS/drag_points.js
+++ _posts/JS/drag_points.js
@@ -1,4 +1,4 @@
-    let svg = d3.select("div#example").selectAll("svg"),
+    let svg = d3.select("#example").selectAll("svg"),
     margin = {top: 20, right: 20, bottom: 30, left: 50},
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom;
